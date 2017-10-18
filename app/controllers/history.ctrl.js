5292527cd0c3acd60206b7a88e36b3510122d9fe
Brooke Wittenberg <brooke.wittenberg@gmail.com>
2017-10-17 19:30:57 -0500

    fixed a dropdown

--- app/controllers/history.ctrl.js
+++ app/controllers/history.ctrl.js
@@ -22,7 +22,7 @@ app.controller("historyCtrl", function($scope, $window, $location, historyFactor
     $scope.medications = [];
     console.log("medications array", $scope.medications);
     $scope.medicationsOptions = [{ id: 1, label: "None" }, { id: 2, label: "Unknown" }, { id: 3, label: "Aspirin" }, { id: 4, label: "Acetominophen" }, { id: 5, label: "Buspar" }, { id: 6, label: "Cumidin" }, { id: 7, label: "Insulin" }, { id: 8, label: "Warfarin" }, { id: 9, label: "Abilify" }, { id: 10, label: "Nexium" }, { id: 11, label: "Humira" }, { id: 12, label: "Crestor" }, { id: 13, label: "Advair Diskus" }, { id: 14, label: "Remicade" }, { id: 15, label: "Cymbalta" }, { id: 16, label: "Copaxone" }, { id: 17, label: "Lantus" }, { id: 18, label: "Januvia" }, { id: 19, label: "Lyrica" }, { id: 20, label: "Oxycontin" }, { id: 21, label: "Celebrex" }, { id: 22, label: "Herceptin" }, { id: 23, label: "Namenda" }, { id: 24, label: "Symbicort" }, { id: 25, label: "Suboxone" }, { id: 26, label: "Seroquel" }, { id: 27, label: "Viagra" }, { id: 28, label: "Cialis" }, { id: 29, label: "Flovent" }, { id: 30, label: "Lunesta" }, { id: 31, label: "Betaseron" }, { id: 32, label: "Simvastatin" }, { id: 33, label: "Omeprazole" }, { id: 34, label: "Metformin" }, { id: 35, label: "Plavix" }, { id: 36, label: "Prozac" }, { id: 37, label: "Zoloft" }, { id: 38, label: "Phenobarbitol" }, { id: 39, label: "Depakote" }];
-    $scope.searchSelectAllSettings = { enableSearch: true, showSelectAll: true, keyboardControls: true, scrollableHeight: '250px', scrollable: true };
+    $scope.searchSelectAllSettings = { enableSearch: true, showSelectAll: true, keyboardControls: true, scrollableHeight: '400px', scrollable: true };
 
     //PAST ILLNESS
     $scope.pastIllness = [];
@@ -39,7 +39,7 @@ app.controller("historyCtrl", function($scope, $window, $location, historyFactor
     //EVENTS LEADING TO CALL
     $scope.eventsTo = [];
     console.log("eventsTo array", $scope.eventsTo);
-    $scope.eventsToOptions = [{ id: 1, label: "Unknown" }, { id: 2, label: "Physical Activity" }, { id: 3, label: "Waking" }, { id: 4, label: "Eating" }, { id: 5, label: "3 Hours" }, { id: 6, label: "4 Hours" }, { id: 7, label: "5 Hours" }, { id: 8, label: "Drug Use" }, { id: 9, label: "Alcohol Use" }, { id: 10, label: "MVA" }, { id: 11, label: "Fall < 20 Feet" }, { id: 12, label: "Fall > 20 Feet" }];
+    $scope.eventsToOptions = [{ id: 1, label: "Unknown" }, { id: 2, label: "Physical Activity" }, { id: 3, label: "Waking" }, { id: 4, label: "Eating" }, { id: 5, label: "Drug Use" }, { id: 6, label: "Alcohol Use" }, { id: 7, label: "MVA" }, { id: 8, label: "Fall < 20 Feet" }, { id: 9, label: "Fall > 20 Feet" }];
     $scope.searchSelectAllSettings = { enableSearch: true, showSelectAll: true, keyboardControls: true, scrollableHeight: '250px', scrollable: true };
     //////////////////////////////
     //Add History Object to FB
